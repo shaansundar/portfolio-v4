9d2476166a36ecfd07809ec4316bfa2f859991c2
shaansundar <shaan.idtindia@gmail.com>
2024-10-03 11:17:34 +0530

    feat: assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'selector',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +9,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'Yellowtail': ['Yellowtail', 'cursive'],
+        'Inter': ['Inter', 'sans-serif'],
       },
+      colors: {
+        'themeBlue': {
+          '1': '#f1fafa',
+          '2': '#dcf1f1',
+          '3': '#bde3e4',
+          '4': '#90ced0',
+          '5': '#64b4b9',
+          '6': '#40949a',
+          '7': '#387982',
+          '8': '#32636c',
+          '9': '#30535a',
+          '10': '#2c464d',
+          '11': '#192e33',
+        },
+      }
     },
   },
   plugins: [],
